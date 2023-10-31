cb87e967831462347e1c20dee132960774d6d16d
tkd-120 <masato11200000@outlook.jp>
2023-10-31 18:11:33 +0900

    test vue

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup></script>
+<template>
+  <p>safariでdisplay:none;が利かないのか確かめます</p>
+  <label>
+    <input class="notWrap" type="radio" />
+    notWrap
+  </label>
+  <label>
+    <input class="" type="radio" />
+    notNone
+  </label>
+
+  <label>
+    <span class="span">
+      <input class="wrap" type="radio" />
+      spanNone
+    </span>
+  </label>
+  <label>
+    <span class="">
+      <input class="" type="radio" />
+      spanNotNone
+    </span>
+  </label>
+</template>
+<style>
+* {
+  text-align: center;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  margin-top: 10px;
+}
+
+label,
+input {
+  cursor: pointer;
+}
+
+.notWrap {
+  display: none;
+}
+
+.span {
+  display: none;
+}
+</style>
